9358cfede22a7c50a9b61f6d87e5318bae1a4839
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-11-21 23:59:31 +0330

    feat(day-countdown-bot): use subscribe method

--- uniquely/day-countdown-bot/src/bot/callback-query/toggleSubscribe.ts
+++ uniquely/day-countdown-bot/src/bot/callback-query/toggleSubscribe.ts
@@ -2,7 +2,7 @@ import {logger} from '../../config.js';
 import {NotStartInlineKeyboard, subscribedStartInlineKeyboard} from '../../content/buttons.js';
 import {message} from '../../director/l18e-loader.js';
 import {bot} from '../../lib/bot.js';
-import {addChat, toggleSubscribe} from '../../util/chat.js';
+import {addChat, subscribe, toggleSubscribe} from '../../util/chat.js';
 
 bot.defineCallbackQueryHandler('toggleSubscribe', async (context) => {
   logger.logMethodArgs?.('command-toggleSubscribe', {chatId: context.chatId});
@@ -16,7 +16,8 @@ bot.defineCallbackQueryHandler('toggleSubscribe', async (context) => {
       context.update.callback_query!.message!.chat,
       context.update.callback_query!.message!.message_thread_id,
     );
-    isSubscribed = await toggleSubscribe(context.chatId);
+    await subscribe(context.chatId);
+    isSubscribed = true;
   }
 
   if (isSubscribed === true) {
